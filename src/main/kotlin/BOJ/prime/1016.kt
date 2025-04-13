56dc6e47b38bb2098edcd1f5a0e8d92f3f3a7ee9
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-04-13 22:30:55 +0900

    소수 관련 문제 풀이

---
+++ src/main/kotlin/BOJ/prime/1016.kt
@@ -0,0 +1,13 @@
+package BOJ.prime
+
+/**
+ * 정수 X - 1보다 큰 제곱수로 나누어 떨어지지 않을 때 -> 제곱 ㄴㄴ 수
+ * 제곱 수 : 4 (2x2) 9 (3x3)
+ */
+
+
+fun main() {
+    var (min,max) = readln().split(" ").map { it.toLong() }
+
+
+}
--- src/main/kotlin/BOJ/prime/1747.kt
+++ src/main/kotlin/BOJ/prime/1747.kt
@@ -3,8 +3,27 @@ package BOJ.prime
 fun main() {
     var n = readln().toInt()
 
-    for (i in 2..1000000) {
+    fun isPrime(x: Int): Boolean {
+        if (x == 1) return false
+        for (i in 2..Math.sqrt(x.toDouble()).toInt()) {
+            if (x%i==0) {
+                return false
+            }
+        }
+        return true
+    }
 
+    fun isPalindrome(x: Int): Boolean {
+        val str = x.toString()
+        return str == str.reversed()
+    }
+
+    while (true) {
+        if (isPrime(n) && isPalindrome(n)) {
+            println(n)
+            break
+        }
+        n++
     }
 
 }
