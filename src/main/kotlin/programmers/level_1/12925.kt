660822c39a7317f47a527248185244c5603e4bcf
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-09-04 16:02:31 +0900

    [Level 1] 문자열을 정수로 바꾸기

---
+++ src/main/kotlin/programmers/level_1/12925.kt
@@ -0,0 +1,11 @@
+package programmers.level_1
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12925?language=kotlin
+
+fun solution12925(s: String): Int {
+    return s.toInt()
+}
+
+fun main() {
+    println(solution12925("12012"))
+}
