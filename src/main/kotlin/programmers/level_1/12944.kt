93ba7e167231d51099def316c53a56761eda9a21
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-09-04 15:49:33 +0900

    [Level 1] 평균 구하기

---
+++ src/main/kotlin/programmers/level_1/12944.kt
@@ -0,0 +1,12 @@
+package programmers.level_1
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12944?language=kotlin
+
+fun solution(arr: IntArray): Double {
+    return arr.average()
+}
+
+
+fun main() {
+    println(solution(intArrayOf(5,5)))
+}
