7f08d60343d9ebef370f8c34bef431627074fd8d
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-09-04 16:20:55 +0900

    [Level 1] 두 정수 사이의 합

---
+++ src/main/kotlin/programmers/level_1/12912.kt
@@ -0,0 +1,26 @@
+package programmers.level_1
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12912?language=kotlin
+
+
+fun solution12912(a: Int, b: Int): Long {
+    var answer: Long = 0
+    if (a > b) {
+        for(i in b..a) {
+            answer += i
+        }
+    } else if (a < b) {
+        for(i in a..b) {
+            answer += i
+        }
+    } else {
+        return a.toLong()
+    }
+    return answer
+}
+
+
+fun main() {
+
+    println(solution12912(3,3))
+}
