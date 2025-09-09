fcf5c59e6c8bd78d8a04c55236828aba05af5390
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-09-09 15:20:48 +0900

    [Level 1] 대충 만든 자판

---
+++ src/main/kotlin/programmers/level_1/160586.kt
@@ -0,0 +1,41 @@
+package programmers.level_1
+
+import kotlin.math.max
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/160586
+
+fun solution160586(keymap: Array<String>, targets: Array<String>): IntArray {
+    var result: IntArray = intArrayOf()
+    for (i in targets.indices) {
+        var arr = targets[i].toCharArray()
+        var count = 0
+        for (j in arr.indices){
+            var min = 101
+            var isCheck = false
+            for (k in keymap.indices){
+                if (keymap[k].contains(arr[j])) {
+                    min = Math.min(min,keymap[k].indexOf(arr[j])+1)
+                    isCheck = true
+                }
+            }
+            if (isCheck) {
+                count+=min
+            } else {
+                count+= 100000
+            }
+        }
+
+        if (count >=100000) {
+            result +=-1
+        } else {
+            result += count
+        }
+    }
+    println(result.joinToString(","))
+    return result
+
+}
+
+fun main() {
+    solution160586(arrayOf("ABACD","BCEFD"), arrayOf("ABCD","AABB"))
+}
