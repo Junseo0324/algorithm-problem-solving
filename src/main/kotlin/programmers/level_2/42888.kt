f873741476e8382c3cfca55ca1b3bbd801756048
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-09-18 19:06:46 +0900

    [Level 2] 오픈 채팅방..

--- src/main/kotlin/programmers/level_2/42888.kt
+++ src/main/kotlin/programmers/level_2/42888.kt
@@ -46,6 +46,7 @@ fun main() {
             arrayOf(
                 "Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"
             )
+
         )
     )
 }
