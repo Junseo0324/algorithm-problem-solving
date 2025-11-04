2dbb089e323928365676c52f20b81b5586c30d64
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-11-04 17:51:38 +0900

    [BOJ/implementation] 4949 균형잡힌 세상

---
+++ src/main/kotlin/BOJ/implementation/4949.kt
@@ -0,0 +1,81 @@
+package BOJ.implementation
+
+// https://www.acmicpc.net/problem/4949
+
+/**
+ * 정민이의 임무는 어떤 문자열이 주어졌을 때, 괄호들의 균형이 잘 맞춰져 있는지 판단하는 프로그램을 짜는 것이다.
+ *
+ * 문자열에 포함되는 괄호는 소괄호("()") 와 대괄호("[]")로 2종류이고, 문자열이 균형을 이루는 조건은 아래와 같다.
+ *
+ * 모든 왼쪽 소괄호("(")는 오른쪽 소괄호(")")와만 짝을 이뤄야 한다.
+ * 모든 왼쪽 대괄호("[")는 오른쪽 대괄호("]")와만 짝을 이뤄야 한다.
+ * 모든 오른쪽 괄호들은 자신과 짝을 이룰 수 있는 왼쪽 괄호가 존재한다.
+ * 모든 괄호들의 짝은 1:1 매칭만 가능하다. 즉, 괄호 하나가 둘 이상의 괄호와 짝지어지지 않는다.
+ * 짝을 이루는 두 괄호가 있을 때, 그 사이에 있는 문자열도 균형이 잡혀야 한다.
+ * 정민이를 도와 문자열이 주어졌을 때 균형잡힌 문자열인지 아닌지를 판단해보자.
+ *
+ * So when I die (the [first] I will see in (heaven) is a score list).
+ * [ first in ] ( first out ).
+ * Half Moon tonight (At least it is better than no Moon at all].
+ * A rope may form )( a trail in a maze.
+ * Help( I[m being held prisoner in a fortune cookie factory)].
+ * ([ (([( [ ] ) ( ) (( ))] )) ]).
+ *  .
+ * .
+ *
+ * yes
+ * yes
+ * no
+ * no
+ * no
+ * yes
+ * yes
+ *
+ * 7번째의 " ."와 같이 괄호가 하나도 없는 경우도 균형잡힌 문자열로 간주할 수 있다.
+ */
+fun main() {
+    val array = mutableListOf<String>()
+    val answer = mutableListOf<String>()
+    while (true) {
+        val input = readln()
+        if (input == ".") break
+        array.add(input)
+    }
+
+    for (str in array) {
+        val stack = mutableListOf<Char>()
+        str.forEach {
+            when (it) {
+                '(' -> stack.add(it)
+                '[' -> stack.add(it)
+                ')' -> {
+                    if (stack.isNotEmpty()) {
+                        if (stack.last() == '(')
+                            stack.removeLast()
+                        else stack.add(it)
+                    } else {
+                        stack.add(it)
+                    }
+                }
+
+                ']' -> {
+                    if (stack.isNotEmpty()) {
+                        if (stack.last() == '[')
+                            stack.removeLast()
+                        else stack.add(it)
+                    } else {
+                        stack.add(it)
+                    }
+                }
+            }
+        }
+        if (stack.isEmpty()) {
+            answer.add("yes")
+        } else {
+            answer.add("no")
+        }
+    }
+
+    println(answer.joinToString("\n"))
+
+}
