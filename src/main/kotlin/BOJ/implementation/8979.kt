342004862010c180096278271312b671c396acd9
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-12-12 18:24:54 +0900

    [BOJ] 8979 올림픽

--- src/main/kotlin/BOJ/implementation/8979.kt
+++ src/main/kotlin/BOJ/implementation/8979.kt
@@ -1,17 +1,44 @@
 package BOJ.implementation
 
 // https://www.acmicpc.net/problem/8979
+
+/**
+ * 1. 금메달이 더 많은 나라
+ * 2. 금메달이 같다면 은메달이 더 많은 나라
+ * 3. 금,은 이 같다면 동메달이 더 많은 나라
+ */
 fun main() {
     val (n, k) = readln().split(" ").map { it.toInt() }
-    val array = Array(n + 1) { IntArray(n) }
-    val answer = IntArray(n+1)
+    val array = Array(n) { IntArray(n) }
     repeat(n) {
-        array[it + 1] = readln().split(" ").map { it.toInt() }.toIntArray()
+        array[it] = readln().split(" ").map { it.toInt() }.toIntArray()
     }
 
-    for (j in 0..<n) { // 금,은,동 확인?
-        for (i in 1..n) {
+    val answerList = array.sortedWith(compareByDescending<IntArray> { it[1] }.thenByDescending { it[2] }.thenByDescending { it[3] })
 
+    val rankedList = mutableListOf<Pair<Int,IntArray>>()
+
+    var currentRank = 1
+    if (answerList.isNotEmpty()) {
+        rankedList.add(Pair(currentRank, answerList[0]))
+    }
+
+
+    for (i in 1 until answerList.size) {
+        val prevItem = answerList[i - 1]
+        val currentItem = answerList[i]
+
+        val isSameRank = compareByDescending<IntArray> { it[1] }.thenByDescending { it[2] }.thenByDescending { it[3] }.compare(currentItem, prevItem) == 0
+
+        if (isSameRank) {
+            rankedList.add(Pair(currentRank, currentItem))
+        } else {
+            currentRank = i + 1
+            rankedList.add(Pair(currentRank, currentItem))
         }
     }
+
+    val targetCountryRank = rankedList.find { it.second[0] == k }?.first
+
+    println(targetCountryRank)
 }
