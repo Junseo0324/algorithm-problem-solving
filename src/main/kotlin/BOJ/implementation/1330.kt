d3810ce810046e02cc94a351299b265aa93d700a
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2026-01-26 19:03:23 +0900

    [BOJ] 1330

---
+++ src/main/kotlin/BOJ/implementation/1330.kt
@@ -0,0 +1,13 @@
+package BOJ.implementation
+
+fun main() {
+    val (a, b) = readln().split(" ").map { it.toInt() }
+
+    if (a > b) {
+        println(">")
+    } else if (a < b) {
+        println("<")
+    } else {
+        println("==")
+    }
+}
