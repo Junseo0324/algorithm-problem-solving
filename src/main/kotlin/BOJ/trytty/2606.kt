671535a870e42ba9ef91c03d9d6886d731cd8f7d
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2026-03-19 22:49:51 +0900

    [BOJ] 2606

---
+++ src/main/kotlin/BOJ/trytty/2606.kt
@@ -0,0 +1,28 @@
+package BOJ.trytty
+
+fun main() {
+    val n = readln().toInt()
+
+    val graph = Array(n+1) { mutableListOf<Int>() }
+    val visited = BooleanArray(n+1)
+
+    repeat(readln().toInt()) {
+        val (f,s) = readln().split(" ").map { it.toInt() }
+        graph[f].add(s)
+        graph[s].add(f)
+    }
+
+    fun dfs(node: Int) {
+        visited[node] = true
+
+        for (next in graph[node]) {
+            if (!visited[next]) {
+                visited[next] = true
+                dfs(next)
+            }
+        }
+    }
+
+    dfs(1)
+    println(visited.count { it }-1)
+}
