442612c3165fcac6e157f2f2dc7df32368a4bcaf
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2026-03-30 15:04:51 +0900

    [programmers] 42883

---
+++ src/main/kotlin/programmers/greedy/42883.kt
@@ -0,0 +1,21 @@
+package programmers.greedy
+
+fun main() {
+    println(solution42883(number = "1924", k = 2))
+    println(solution42883(number = "1231234", k = 3))
+    println(solution42883(number = "4177252841", k = 4))
+}
+
+fun solution42883(number: String, k: Int): String {
+    val stack = ArrayDeque<Char>()
+    var count = k
+
+    for (num in number) {
+        while (stack.isNotEmpty() && count > 0 && stack.last() < num) {
+            stack.removeLast()
+            count--
+        }
+        stack.addLast(num)
+    }
+    return stack.take(number.length - k).joinToString("")
+}
