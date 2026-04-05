a6f9bc50fc721720cb761ddbcb79bd03032cbe2d
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2026-04-05 19:54:10 +0900

    [BOJ] 5347

---
+++ src/main/kotlin/BOJ/euclidean/5347.kt
@@ -0,0 +1,15 @@
+package BOJ.euclidean
+
+fun main() {
+    fun gcd(a: Long, b: Long): Long {
+        return if (b == 0L) a else gcd(b, a % b)
+    }
+
+    fun lcm(a: Long, b: Long): Long {
+        return Math.abs(a * b) / gcd(a, b)
+    }
+    repeat(readln().toInt()) {
+        val (a, b) = readln().split(" ").map { it.toLong() }
+        println(lcm(a,b))
+    }
+}
