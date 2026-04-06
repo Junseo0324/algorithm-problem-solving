5dfb733ab77d8c7e31c0d70de727140d6efb2f7c
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2026-04-06 11:02:32 +0900

    [programmers] 70128

---
+++ src/main/kotlin/programmers/practice04/70128.kt
@@ -0,0 +1,9 @@
+package programmers.practice04
+
+fun solution70128(a: IntArray, b: IntArray): Int {
+    var answer = 0
+    for (i in a.indices) {
+        answer += a[i] * b[i]
+    }
+    return answer
+}
