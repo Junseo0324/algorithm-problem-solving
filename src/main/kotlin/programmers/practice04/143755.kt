4623714127622bd03ed1f7fb425dd17250d11125
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2026-04-06 11:37:18 +0900

    [programmers] 143755

---
+++ src/main/kotlin/programmers/practice04/143755.kt
@@ -0,0 +1,18 @@
+package programmers.practice04
+
+fun main() {
+    println(solution143755("3141592", "271"))
+    println(solution143755("500220839878", "7"))
+    println(solution143755("10203", "15"))
+}
+
+fun solution143755(t: String, p: String): Int {
+    var answer = 0
+    val size = p.length
+    for (i in 0..t.length - size) {
+        if (t.substring(i, i + size) <= p) {
+            answer++
+        }
+    }
+    return answer
+}
