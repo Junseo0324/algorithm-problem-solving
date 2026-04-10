b6f5059ed0887d0e678269ea515ada1cd2f38c5a
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2026-04-11 02:18:23 +0900

    [programmers] 42862

---
+++ src/main/kotlin/programmers/practice04/42862.kt
@@ -0,0 +1,26 @@
+package programmers.practice04
+
+fun main() {
+    println(solution42862(5, intArrayOf(2, 4), intArrayOf(1, 3, 5)))
+    println(solution42862(5, intArrayOf(2, 4), intArrayOf(3)))
+    println(solution42862(3, intArrayOf(3), intArrayOf(1)))
+    println(solution42862(3, intArrayOf(3), intArrayOf(1, 3)))
+}
+
+fun solution42862(n: Int, lost: IntArray, reserve: IntArray): Int {
+    val lostList = lost.filter { it !in reserve }.sorted()
+    val reserveList = reserve.filter { it !in lost }.toMutableList()
+
+    var lostCount = lostList.size
+    for (student in lostList) {
+        if (student - 1 in reserveList) {
+            reserveList.remove(student - 1)
+            lostCount--
+        } else if (student + 1 in reserveList) {
+            reserveList.remove(student + 1)
+            lostCount--
+        }
+    }
+
+    return n - lostCount
+}
