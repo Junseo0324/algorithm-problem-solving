9307a5466e1b3c54a7f2068e4f1b6dbdbe5f39dd
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2026-04-12 00:16:32 +0900

    [programmers] 64061

---
+++ src/main/kotlin/programmers/practice04/64061.kt
@@ -0,0 +1,43 @@
+package programmers.practice04
+
+fun main() {
+    println(
+        solution64061(
+            board = arrayOf(
+                intArrayOf(0, 0, 0, 0, 0),
+                intArrayOf(0, 0, 1, 0, 3),
+                intArrayOf(0, 2, 5, 0, 1),
+                intArrayOf(4, 2, 4, 4, 2),
+                intArrayOf(3, 5, 1, 3, 1),
+            ),
+            moves = intArrayOf(1, 5, 3, 5, 1, 2, 1, 4)
+        )
+    )
+}
+
+fun solution64061(board: Array<IntArray>, moves: IntArray): Int {
+    var answer = 0
+    val n = board.size
+    val stack = mutableListOf<Int>()
+    val copyBoard = board
+    for (move in moves) {
+        for (i in 0 until n) {
+            if (copyBoard[i][move-1] != 0) {
+                if (stack.isNotEmpty()) {
+                    if (stack.last() == copyBoard[i][move-1]) {
+                        stack.removeLast()
+                        answer +=2
+                    } else {
+                        stack.add(copyBoard[i][move-1])
+                    }
+                } else {
+                    stack.add(copyBoard[i][move-1])
+                }
+                copyBoard[i][move-1] = 0
+                break
+            }
+        }
+    }
+
+    return answer
+}
